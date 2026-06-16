a0daf192e45e06d994fcc6ce0f9b5b4911d5dd3b
Rahul <toxicrega07@gmail.com>
2026-06-16 20:14:23 +0530

    Import stopwords and update NLTK packages
    Added import for NLTK stopwords and updated package list.

--- setup_nltk.py
+++ setup_nltk.py
@@ -4,6 +4,7 @@ Download required NLTK data packages.
 Run once before starting the server: python setup_nltk.py
 """
 import nltk
+from nltk.corpus import stopwords
 
 packages = ["punkt", "stopwords", "punkt_tab"]
 for pkg in packages:
